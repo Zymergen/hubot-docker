001208881832dd1e11cda590d097ed4260b4d391
glonkar <glonkar@zymergen.com>
2018-09-04 16:46:46 -0700

    remove unwanted

--- terraform/key-pairs.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_key_pair" "deployer" {
-  key_name = "trainer_sean_kane_hubot"
-  public_key = "${file(var.ssh_public_key_path)}"
-}
--- terraform/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "server.ip" {
-  value = "${aws_instance.server.public_ip}"
-}
